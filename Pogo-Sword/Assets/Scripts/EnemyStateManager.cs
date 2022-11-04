9ba22a26e641bc1ddb8a29c2aab9e2539edc7c5d
whc823 <wanghuac@uci.edu>
2022-11-03 20:12:49 -0700

    changes

---
+++ Pogo-Sword/Assets/Scripts/EnemyStateManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateManager : MonoBehaviour
+{
+    public enum CurrentState
+    {
+        Idle,
+        Patrolling,
+        Dead
+
+
+    }
+
+    public static CurrentState state = CurrentState.Idle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(state);
+    }
+}
